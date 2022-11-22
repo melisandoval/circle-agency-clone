21800e2d4424241a3341c8fcdb510629625f77d9
melisand <melisandoval@live.com.ar>
2022-11-22 20:23:35 +0100

    add suscribe response

--- index.js
+++ index.js
@@ -17,6 +17,9 @@ function sendEmailtoDb(e) {
   const emailRegex =
     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
+  const formDiv = document.getElementById("form-container");
+  const sectionParagraph = document.getElementById("cta-p");
+
   if (emailRegex.test(email)) {
     db.collection("emails")
       .add({
@@ -24,6 +27,9 @@ function sendEmailtoDb(e) {
       })
       .then((docRef) => {
         console.log("Document written with ID: ", docRef.id);
+        sectionParagraph.innerHTML = "";
+        formDiv.innerHTML =
+          "<p class='suscribe-response'>Your email has been received 🎉<br> We will contact you very soon!</p>";
       })
       .catch((error) => {
         console.error("Error adding document: ", error);
