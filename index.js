25e7d1e25667fe8f00917e9f368201efed99db10
melisand <melisandoval@live.com.ar>
2022-11-22 02:05:58 +0100

    add regex test

--- index.js
+++ index.js
@@ -12,19 +12,23 @@ const db = firebase.initializeApp(firebaseConfig).firestore();
 
 function sendEmailtoDb(e) {
   const email = document.getElementById("email").value;
-  console.log(email);
   e.preventDefault();
 
-  db.collection("emails")
-    .add({
-      email: email,
-    })
-    .then((docRef) => {
-      console.log("Document written with ID: ", docRef.id);
-    })
-    .catch((error) => {
-      console.error("Error adding document: ", error);
-    });
+  const emailRegex =
+    /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
+
+  if (emailRegex.test(email)) {
+    db.collection("emails")
+      .add({
+        email: email,
+      })
+      .then((docRef) => {
+        console.log("Document written with ID: ", docRef.id);
+      })
+      .catch((error) => {
+        console.error("Error adding document: ", error);
+      });
+  } else console.log("Please write a valid email");
 }
 // Add a new email to Firestore with a generated id.
 
