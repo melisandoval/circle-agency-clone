f324a8fd0dcdab1814a14e3e6a7ac7070b820eb1
melisand <melisandoval@live.com.ar>
2022-11-22 20:41:33 +0100

    add error messages to suscribe form

--- index.js
+++ index.js
@@ -11,32 +11,32 @@ const firebaseConfig = {
 const db = firebase.initializeApp(firebaseConfig).firestore();
 
 function sendEmailtoDb(e) {
-  const email = document.getElementById("email").value;
   e.preventDefault();
-
+  const email = document.getElementById("email").value;
   const emailRegex =
     /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
   const formDiv = document.getElementById("form-container");
   const sectionParagraph = document.getElementById("cta-p");
+  const invalidEmailMessage = document.getElementById("invalid-email-message");
+  const errorMessage = document.getElementById("error-message");
 
   if (emailRegex.test(email)) {
     db.collection("emails")
       .add({
         email: email,
       })
-      .then((docRef) => {
-        console.log("Document written with ID: ", docRef.id);
+      .then(() => {
         sectionParagraph.innerHTML = "";
         formDiv.innerHTML =
           "<p class='suscribe-response'>Your email has been received 🎉<br> We will contact you very soon!</p>";
       })
       .catch((error) => {
         console.error("Error adding document: ", error);
+        errorMessage.removeAttribute("hidden");
       });
-  } else console.log("Please write a valid email");
+  } else invalidEmailMessage.removeAttribute("hidden");
 }
-// Add a new email to Firestore with a generated id.
 
 window.addEventListener("load", () => {
   const suscribeBtn = document.getElementById("suscribe");
